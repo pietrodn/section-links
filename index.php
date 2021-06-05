0da5c1baefd2de991a930a240d626ee28eb35c3c
pietrodn <powerpdn@gmail.com>
2021-06-05 23:10:18 +0200

    fix: broken URL

--- index.php
+++ index.php
@@ -47,7 +47,7 @@
 			<li class="active"><a href=".">Section Links</a></li>
 			<li><a href="//github.com/pietrodn/section-links">Source (GitHub)</a></li>
 			<li><a href="//wikitech.wikimedia.org/wiki/User:Pietrodn">Pietrodn</a></li>
-			<li><a href="../intersect-contribs/">Intersect Contribs</a></li>
+			<li><a href="//intersect-contribs.toolforge.org/">Intersect Contribs</a></li>
 		  </ul>
 		</div><!--/.nav-collapse -->
 	  </div>
@@ -74,7 +74,7 @@
 				<li><em>To</em> option is quite slow and expensive: it needs to retrieve the text of every reference to the given page (it frequently hits the API call limit).</li>
 				<li>Redirect handling is inconsistent.</li>
 			</ul>
-		
+
 			<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="get">
 				<div class="form-group">
 					<label for="wikiDb">Project</label>:
@@ -85,7 +85,7 @@
 						projectChooser($selectedProject);
 					?>
 					</select>
-				
+
 					<?php
 						$directions = Array('from'=>'From: ', 'to'=>'To: ');
 						foreach($directions as $i=>$label)
@@ -99,8 +99,8 @@
 							echo '</div>';
 						}
 					?>
-				
-					<input class="form-control" type="text" placeholder="Page title" required name="wikiPage" value="<?php 
+
+					<input class="form-control" type="text" placeholder="Page title" required name="wikiPage" value="<?php
 						if(isset($_GET['wikiPage']))
 						print htmlentities($_GET['wikiPage'], ENT_QUOTES, 'UTF-8'); ?>" />
 				</div>
@@ -131,16 +131,16 @@
 			throw new Exception('You tried to select a non-existent wiki!', NONEXISTENT_WIKI_EXC);
 		if(!in_array($_GET['wikiDirection'], array_keys($directions)))
 			throw new Exception('Error specifying the direction.', DIRECTION_EXC);
-	
+
 		$list = new SectionLinkList($wikiHost);
 		$ss = new SectionStore($wikiHost);
-	
+
 		$pageForUrl = rawurlencode($_GET['wikiPage']);
 		$pageText = getPageText($_GET['wikiPage'], FALSE);
 		if($pageText===FALSE)
 			throw new Exception("The page that you entered doesn't exist in this wiki.");
-		
-		if($_GET['wikiDirection'] == 'from') {            
+
+		if($_GET['wikiDirection'] == 'from') {
 			preg_match_all('/\[\[([^\]|]*)#([^\]|]+)(\|[^\]]+)?\]\]/', $pageText, $linkedMatches, PREG_SET_ORDER);
 			foreach($linkedMatches as $linkedMatch) {
 				$linkedPage = $linkedMatch[1];
@@ -148,9 +148,9 @@
 				if($linkedPage == '') {
 					$linkedPage = $_GET['wikiPage'];
 				}
-			
+
 				$flags = 0;
-			
+
 				$result = $ss->hasSection($linkedSect, $linkedPage);
 				if($result != SectionStore::SECTION_YES) {
 					if($result == SectionStore::RED_LINK) {
@@ -161,31 +161,31 @@
 					$list->add($_GET['wikiPage'], $linkedPage, $linkedSect, $flags);
 				}
 			}
-		
+
 		} else if($_GET['wikiDirection'] == 'to') {
 			// Links pointing to the specified page
 			echo '<ul>';
-		
+
 			// Sections of specified page
 			$mySections = $ss->getSections($_GET['wikiPage']);
-		
+
 			// Get references
 			$references = getReferences($_GET['wikiPage']);
 			$references[] = array('title' => $_GET['wikiPage'], 'revisions' => array(0 => array('*' => $pageText))); // Adding the start page page as possible reference, as it's not included in backreferences.
-		
+
 			foreach($references as $row) {
 				$refPage = $row['title'];
 				$refText = $row['revisions'][0]['*'];
 				$pageForRegexp = preg_replace('/[ _]/', '[ _]', preg_quote($_GET['wikiPage'], '/'));
-				preg_match_all('/\[\[(' . $pageForRegexp . ')' . 
+				preg_match_all('/\[\[(' . $pageForRegexp . ')' .
 					($refPage == $_GET['wikiPage'] ? '?' : '') . // Destination page can be omitted only if source = dest.
 					'#([^\]|]+)(\|[^\]]+)?\]\]/',
-					$refText, $refMatches, PREG_SET_ORDER);  
-			
+					$refText, $refMatches, PREG_SET_ORDER);
+
 				foreach($refMatches as $refMatch) {
 					$flags = 0;
 					$refSect = str_replace('_', ' ', rawurldecode($refMatch[2]));
-				
+
 					if(!in_array($refSect, $mySections) || $_GET['wikiPage'] == $refPage)
 					{
 						if(hasAnchor($refSect, $pageText))
@@ -197,7 +197,7 @@
 				}
 			}
 		}
-	
+
 		$count = $list->countList();
 		if($count === 0) {
 			echo '<div class="alert alert-info">';
@@ -211,10 +211,10 @@
 				echo "Links to inexistent sections pointing to the specified <a href=\"//$wikiHost/wiki/$pageForUrl\">page</a> (<a href=\"//$wikiHost/w/index.php?title=$pageForUrl&amp;action=edit\">edit</a>):\n";
 			}
 			echo $count . ' results found.';
-			echo '</div>';	
+			echo '</div>';
 			$list->printList();
 		}
-	
+
 	} catch (Exception $e) {
 		if(!($e->getCode() == ALL_PARAMS_MISSING_EXC))
 			printError($e->getMessage());
@@ -229,7 +229,7 @@
 		global $wikiHost, $apiCalls;
 		if(++$apiCalls > MAX_API_CALLS)
 			throw new Exception('Max. API call limit exceeded (' . MAX_API_CALLS . ')', API_EXC);
-	
+
 		$pageForUrl = rawurlencode($page);
 		$url = "https://$wikiHost/w/api.php?action=query&prop=revisions&generator=backlinks&gbltitle=$pageForUrl$queryContinue&rvprop=content&format=json";
 		$req = curl_init($url);
@@ -243,7 +243,7 @@
 			$qc = '&' . http_build_query($unser['continue']);
 			return array_merge($references, getReferences($page, $qc));
 		} else {
-		
+
 			return $references;
 		}
 	}
@@ -262,18 +262,18 @@
 	{
 		global $wikiHost, $apiCalls;
 		static $textCache = array();
-	
+
 		if(array_key_exists($page, $textCache))
 		{
 			return $textCache[$page];
 		}
-	
+
 		if(++$apiCalls > MAX_API_CALLS)
 			throw new Exception('Max. API call limit exceeded (' . MAX_API_CALLS . ')', API_EXC);
-		
+
 		$redirects = '';
 		if($followRedirects)
-			$redirects='&redirects'; 
+			$redirects='&redirects';
 		$pageForUrl = rawurlencode($page);
 		$req = curl_init("https://$wikiHost/w/api.php?action=query&prop=revisions&titles=$pageForUrl&rvprop=content$redirects&format=json");
 		curl_setopt($req, CURLOPT_RETURNTRANSFER, 1);
@@ -300,11 +300,11 @@
 			</p>
 		</div>
 	</div>
-	
+
 	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="//tools-static.wmflabs.org/cdnjs/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="//tools-static.wmflabs.org/cdnjs/ajax/libs/twitter-bootstrap/3.3.5/js/bootstrap.min.js"></script>
-    
+
 	</body>
 </html>
